57b92c5fcf4ea20cacd08695744b22ac60999563
ParkInfo <ragull@iMac.local>
2016-10-26 14:32:46 +0530

    sample

--- SampleCaLayer/ViewController.m
+++ SampleCaLayer/ViewController.m
@@ -1,4 +1,4 @@
-//
+	//
 //  ViewController.m
 //  SampleCaLayer
 //
@@ -9,15 +9,65 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIView *ViewForLayer;
 
 @end
 
 @implementation ViewController
 
+-(id) l: CALayer {
+    return _ViewForLayer.layer;
+}
+
+-(CALayer*)ll
+{
+    return _ViewForLayer.layer;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self setUpLayer];
+     [self setUpLayer123];
 }
+
+-(void) setUpLayer {
+    
+
+    CALayer *layerr=self.ViewForLayer.layer;
+    layerr.backgroundColor=[UIColor blueColor].CGColor;
+    layerr.borderWidth=100.0;
+    layerr.borderColor=[UIColor redColor].CGColor;
+    layerr.shadowOpacity = 0.7;
+    layerr.shadowRadius = 10.0;
+    layerr.contents=(__bridge id _Nullable)([UIImage imageNamed:@"unnamed.png"].CGImage);
+    layerr.contentsGravity=kCAGravityCenter;
+    
+    
+}
+-(void) setUpLayer123 {
+    
+    
+    CALayer *layerr=self.ViewForLayer.layer;
+    layerr.backgroundColor=[UIColor blueColor].CGColor;
+    layerr.borderWidth=100.0;
+    layerr.borderColor=[UIColor redColor].CGColor;
+    layerr.shadowOpacity = 0.7;
+    layerr.shadowRadius = 10.0;
+    layerr.contents=(__bridge id _Nullable)([UIImage imageNamed:@"unnamed.png"].CGImage);
+    layerr.contentsGravity=kCAGravityCenter;
+    
+    
+}
+- (IBAction)tapGestureRecoganized:(UITapGestureRecognizer *)sender {
+    CALayer *layerr=self.ViewForLayer.layer;
+    layerr.shadowOpacity=layerr.shadowOpacity==0.7 ? 0.0 : 0.7;
+}
+
+- (IBAction)pinchGestureRecoganized:(UIPinchGestureRecognizer *)sender {
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
